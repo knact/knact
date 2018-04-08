9adcd8ab5461e819ee86b514038777c295cbf362
Nashe <mncubenashe@gmail.com>
2018-04-08 19:07:39 +0100

    Procedure repo done, node repo partially defined. Not sure on package scala definitons. Read TODO

--- guard-server/src/main/scala/io/knact/guard/server/H2JdbcContext.scala
+++ guard-server/src/main/scala/io/knact/guard/server/H2JdbcContext.scala
@@ -25,13 +25,12 @@ class H2JdbcContext extends ApiContext {
 	// DONE Create table uniquely on first call and keep
 	// DONE Define methods for modifying and inspecting the database
 	// DONE Decide how to store logs.
-  // TOOD: Smart functions for data manipulation i.e joins etc
 
 
 	val dbPath = Paths.get(".test").toAbsolutePath
 
-	override def nodes: NodeRepository = ???
-	override def procedures: ProcedureRepository = ???
+	override def nodes: NodeRepository = new Nodes()
+	override def procedures: ProcedureRepository = new Procedures()
 
 
 	val xa = Transactor.fromDriverManager[Task](
@@ -95,7 +94,7 @@ class H2JdbcContext extends ApiContext {
 
 	//println(ddl.transact(xa).unsafeRunSync)
 
-  def upsertNode(node: Node) = {
+  def upsertNode(node: Node): Task[Id[Node]] = {
     // Note: ID can be none
     val id, host, port, remark, logs  =
       node.id,
@@ -109,16 +108,16 @@ class H2JdbcContext extends ApiContext {
         for {
           id <- sql"""insert into nodes(id, host, port, remark, logs) values($host, $port, $remark, $logs)"""
           .update
-          .withUniqueGeneratedKeys[Long] ("id")
+          .withUniqueGeneratedKeys[Long] ("id").transact(xa)
         } yield (id)
       case _ =>
         for {
-          _ <- sql"""update nodes set host=$host, port=$port, remark=$remark, logs=$logs where id=$id """
-        } yield()
+          _ <- sql"""update nodes set host=$host, port=$port, remark=$remark, logs=$logs where id=$id """.update.transact(xa)
+        } yield(id)
     }
   }
 
-  def upsertProcedure(proc: Procedure) = {
+  def upsertProcedure(proc: Procedure): Task[Id[Procedure]] =  {
     val id, desc, code, duration =
       proc.id,
       proc.desc,
@@ -130,22 +129,86 @@ class H2JdbcContext extends ApiContext {
         for {
           id <- sql"""insert into procedures(desc, code, duration) values($desc, $code, $duration)"""
             .update
-            .withUniqueGeneratedKeys[Long]("id")
+            .withUniqueGeneratedKeys[Long]("id").transact(xa)
         } yield (id)
       case _ =>
         for {
-          _ <- sql"""update procedures set desc=$desc, code=$code, duration=$duration where id=$id """.update
-        } yield ()
+          _ <- sql"""update procedures set desc=$desc, code=$code, duration=$duration where id=$id """.update.transact(xa)
+        } yield (id)
     }
   }
 
-  def selectNodes():Query0[(Long, String, Int, String, String)] = {
-    sql"select id, host, port, remark, logs from nodes".query[(Long, String, Int, String, String)].to[List].transact(xa).unsafeRunSync
+  def selectNodes():Task[Seq[(Long, String, Int, String, String)]] = {
+    sql"select id, host, port, remark, logs from nodes".query[(Long, String, Int, String, String)].to[Seq].transact(xa)
   }
 
-  def selectProcedures(): List[(Long, String, String, Long)] = {
-    sql"select id, desc, code, duration from procedures".query[(Long, String, String, Long)].to[List].transact(xa).unsafeRunSync
+  def selectProcedures(): Task[Seq[Procedure]] = {
+    sql"select id, desc, code, duration from procedures".query[Procedure].to[Seq].transact(xa)
   }
+
+  class Procedures() extends(ProcedureRepository) = {
+    def list(): Task[Seq[Id[Procedure]]] = {
+      sql"select id from PROCEDURES".query[Id[Procedure]].toSeq.transact(xa)
+    }
+
+    def find(id: Id[Procedure]): Task[Option[Procedure]] ={
+      sql"select id, desc, code, duration from PROCEDURES where id=%id".query[Option[Procedure]].transact(xa)
+    }
+
+    def delete(id:Id[Procedure]): Task[Id[Procedure]] = {
+      sql"delete from procedures where id=%id".query[Option[Procedure]].transact(xa) //TODO Optional | Failure
+    }
+
+    def insert(p: Procedure) = Id[Procedure] = {
+      upsertProcedure(p).asyncBoundary
+    }
+
+    def update(id: Id[Procedure], f: Procedure=>Procedure): Task[Id[Procedure]] = {
+      upsertProcedure(id)
+    }
+
+  }
+
+  class Nodes extends(NodeRepository) = {
+
+    def list(): Task[Seq[Id[Node]]] = {
+      sql"select id from nodes".query[Id[Node]].toSeq.transact(xa)
+    }
+
+    def find(id: Id[Node]): Task[Option[Node]] ={
+      sql"select id, desc, code, duration from nodes where id=%id".query[Option[Node]].transact(xa)
+    }
+
+    def delete(id:Id[Node]): Task[Id[Node]] = {
+      sql"delete from nodes where id=%id".query[Option[Node]].transact(xa) //TODO Optional | Failure
+    }
+
+    def insert(n: Node) = Id[Node] = {
+      upsertNode(n).asyncBoundary
+    }
+
+    def update(id: Id[Node], f: Node=>Node): Task[Id[Node]] = {
+      upsertNode(id)
+    }
+
+    def ids: Observable[Set[Id[Node]]] = {
+      sql"select id from nodes".query[Id[Node]].toSet.transact(xa)
+    }
+
+    // TODO:What are the delta functions?
+    // TODO: What are the persist functions?
+    // TODO: What is the Execute function?
+
+    def telemetries(nid: Id[Node]): Task[Option[TelemetrySeries]] = {
+      sql"select verdict from telemetry where id=$nid".query[Option[TelemetrySeriies]].transact(xa)
+    }
+
+    def logs(nid: Id[Node]): Task[Option[LogSeries]] = {
+      sql"select lineID, lineVal from logs where nodeID=%nid".query[Option[LogSeries]].transact(xa)
+    }
+
+  }
+
 
 
 
