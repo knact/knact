40ead5ccfda19abeeb5dcb71986cd9c983ebdb8c
Nashe Mncube <nm15042@my.bristol.ac.uk>
2018-04-18 12:49:25 +0100

    Progress made

--- guard-server/src/main/scala/io/knact/guard/server/H2JdbcContext.scala
+++ guard-server/src/main/scala/io/knact/guard/server/H2JdbcContext.scala
@@ -1,204 +1,352 @@
-//package io.knact.guard.server
-//
-//
-//import java.nio.file.Paths
-//
-//import doobie._
-//import doobie.implicits._
-//import io.knact.guard.Entity.{Id, LogSeries, Node, Procedure, TelemetrySeries}
-//import io.knact.guard._
-//import monix.reactive.Observable
-//// In order to evaluate tasks, we'll need a Scheduler
-//import monix.execution.Scheduler.Implicits.global
-//// Task is in monix.eval
-//import cats.implicits._
-//import monix.eval.Task
-//
-//class H2JdbcContext extends ApiContext {
-//
-//	// DONE insert nodes
-//	// DONE defining procedure table
-//	// DONE Create table uniquely on first call and keep
-//	// DONE Define methods for modifying and inspecting the database
-//	// DONE Decide how to store logs.
-//
-//
-//	private val dbPath = Paths.get(".test").toAbsolutePath
-//
-//	override def nodes: NodeRepository = new Nodes()
-//	override def procedures: ProcedureRepository = new Procedures()
-//
-//
-//	val xa = Transactor.fromDriverManager[Task](
-//		"org.h2.driver",
-//		s"jdbc:h2:$dbPath"
-//	)
-//
-//	private val ddl = for {
-//		nodes <-
-//			sql"""
-//                    IF NOT EXISTS(SELECT * FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME='NODE')
-//                    BEGIN
-//                      CREATE TABLE NODES(
-//                      id BIGINT,
-//                      host VARCHAR NOT NULL,
-//                      port INT NOT NULL,
-//                      remark VARCHAR,
-//                      logLine BIGINT,
-//                      telemetry BIGINT,
-//                      PRIMARY KEY (id),
-//                      FOREIGN KEY (logLine) REFERENCES LOGS(lineID)
-//                      FOREIGN KEY (telemetry) REFERENCES TELEMETRY(id)
-//                      )
-//                    END
-//            """.update.run
-//
-//		procedures <-
-//			sql"""
-//                    IF NOT EXISTS(SELECT * FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME='PROCEDURE')
-//                    BEGIN
-//                      CREATE TABLE PROCEDURES(
-//                      id BIGINT,
-//                      desc VARCHAR,
-//                      code VARCHAR,
-//                      duration BIGINT
-//                      )
-//                    END""".update.run // TODO: How best to stor duration
-//
-//		logs <-
-//			sql"""
-//                   IF NOT EXISTS (SELECT * FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME='LOGS')
-//                   BEGIN
-//                      CREATE TABLE LOGS(
-//                      lineID BIGINT,
-//                      lineVal VARCHAR,
-//                      nodeID BIGINT,
-//                      PRIMARY KEY(lineID),
-//                      FOREIGN KEY (nodeID) REFERENCES NODE(id)
-//                      )
-//                   END   """.update.run
-//
-//		telemetry <-
-//			sql"""
-//                   IF NOT EXISTS (SELECT * FROM INFORMATION_SCHEMA WHERE TABLE_NAME='TELEMETRY')
-//                       BEGIN
-//                          CREATE  TABLE TELEMETRY(
-//                          id BIGINT,
-//                          verdict VARCHAR,
-//                          PRIMARY KEY(id),
-//                          FOREIGN KEY (id) REFERENCES NODES(id)
-//                          )
-//                       END
-//                           """.update.run
-//	} yield (nodes, procedures, logs, telemetry)
-//
-//	//println(ddl.transact(xa).unsafeRunSync)
-//
-//	def upsertNode(node: Node): Task[Id[Node]] = {
-//		// Note: ID can be none
-//		val Node(idOp, target, remark, _, logs) = node
-//		val host = target.host
-//		val port = target.port
-//		idOp match {
-//			case None     =>
-//				sql"""insert into nodes(id, host, port, remark, logs) values($host, $port, $remark, $logs)"""
-//					.update
-//					.withUniqueGeneratedKeys[Long]("id").transact(xa).map(id => id)
-//			case Some(id) =>
-//				sql"""update nodes set host=$host, port=$port, remark=$remark, logs=$logs where id=$id """
-//					.update.transact(xa)
-//					.map { case _ => id }
-//		}
-//	}
-//
-//	def upsertProcedure(proc: Procedure): Task[Id[Procedure]] = {
-//		val Procedure(idOp, _, desc, code, duration) = proc
-//		idOp match {
-//			case None     => sql"""insert into procedures(desc, code, duration) values($desc, $code, $duration)"""
-//				.update
-//				.withUniqueGeneratedKeys[Long]("id").transact(xa).map(id => id)
-//			case Some(id) => sql"""update procedures set desc=$desc, code=$code, duration=$duration where id=$id """
-//				.update
-//				.transact(xa)
-//				.map { case _ => id }
-//		}
-//	}
-//
-//	def selectNodes(): Task[Seq[(Long, String, Int, String, String)]] = {
-//		sql"select id, host, port, remark, logs from nodes"
-//			.query[(Long, String, Int, String, String)]
-//			.to[Seq].transact(xa)
-//	}
-//
-//	def selectProcedures(): Task[Seq[Procedure]] = {
-//		sql"select id, desc, code, duration from procedures".
-//			query[Procedure].
-//			to[Seq].transact(xa)
-//	}
-//
-//	class Procedures  extends ProcedureRepository {
-//
-//		def list(): Task[Seq[Entity.Id[Procedure]]] = {
-//			sql"select id from PROCEDURES".query[Id[Procedure]].toSeq.transact(xa)
-//		}
-//
-//		def find(id: Entity.Id[Procedure]): Task[Option[Procedure]] = {
-//			sql"select id, desc, code, duration from PROCEDURES where id=%id".query[Option[Procedure]].transact(xa)
-//		}
-//
-//		def delete(id: Entity.Id[Procedure]): Task[Entity.Id[Procedure]] = {
-//			sql"delete from procedures where id=%id".query[Option[Procedure]].transact(xa) //TODO Optional | Failure
-//		}
-//
-//		def insert(p: Procedure) = Entity.Id[Procedure] = {
-//			upsertProcedure(p).asyncBoundary
-//		}
-//
-//		def update(id: Id[Procedure], f: Procedure => Procedure): Task[Id[Procedure]] = {
-//			upsertProcedure(id)
-//		}
-//
-//	}
-//
-//	class Nodes extends NodeRepository {
-//
-//		def list(): Task[Seq[Id[Node]]] = {
-//			sql"select id from nodes".query[Entity.Id[Node]].toSeq.transact(xa)
-//		}
-//
-//		def find(id: Entity.Id[Node]): Task[Option[Node]] = {
-//			sql"select id, desc, code, duration from nodes where id=%id".query[Option[Node]].transact(xa)
-//		}
-//
-//		def delete(id: Id[Entity.Node]): Task[Entity.Id[Node]] = {
-//			sql"delete from nodes where id=%id".query[Option[Node]].transact(xa) //TODO Optional | Failure
-//		}
-//
-//		def insert(n: Node) = Id[Node] = {
-//			upsertNode(n).asyncBoundary
-//		}
-//
-//		def update(id: Id[Node], f: Node => Node): Task[Id[Node]] = {
-//			upsertNode(id)
-//		}
-//
-//		def ids: Observable[Set[Id[Node]]] = {
-//			sql"select id from nodes".query[Id[Node]].toSet.transact(xa)
-//		}
-//
-//		// TODO:What are the delta functions?
-//		// TODO: What are the persist functions?
-//		// TODO: What is the Execute function?
-//
-//		def telemetries(nid: Id[Node]): Task[Option[TelemetrySeries]] = {
-//			sql"select verdict from telemetry where id=$nid".query[Option[TelemetrySeries]].transact(xa)
-//		}
-//
-//		def logs(nid: Id[Node]): Task[Option[LogSeries]] = {
-//			sql"select lineID, lineVal from logs where nodeID=$nid".query[Option[LogSeries]].transact(xa)
-//		}
-//
-//	}
-//
-//
-//}
+package io.knact.guard.server
+
+
+import java.nio.file.Paths
+import java.time.{Duration, ZonedDateTime}
+
+import doobie._
+import doobie.implicits._
+import io.knact.guard.Entity.{Id, LogSeries, Node, Procedure, SshKeyTarget, SshPasswordTarget, TelemetrySeries, TimeSeries}
+import io.knact.guard._
+import io.knact.guard.Telemetry.{Critical, DiskStat, Error, Iface, MemoryStat, NetStat, Offline, Ok, Online, Status, ThreadStat, Timeout, Verdict, Warning}
+import monix.reactive.Observable
+import squants.information.{Information, InformationUnit}
+// In order to evaluate tasks, we'll need a Scheduler
+import monix.execution.Scheduler.Implicits.global
+// Task is in monix.eval
+import cats.implicits._
+import monix.eval.Task
+
+class H2JdbcContext extends ApiContext {
+
+  //DONE: Telemetry base
+  //TODO: Entitity mappings
+  //TODO: Relevant entitity and SQL commands
+  override def version: String = ???
+
+  override def repos: (NodeRepository, ProcedureRepository) = ???
+
+  override def startTime: ZonedDateTime = ???
+
+
+  private val dbPath = Paths.get("mem:test").toAbsolutePath
+
+  override def nodes: NodeRepository = ??? //new Nodes()
+  override def procedures: ProcedureRepository = ??? //new Procedures()
+
+
+  val xa = Transactor.fromDriverManager[Task](
+    "org.h2.driver",
+    s"jdbc:h2:$dbPath"
+  )
+
+  private val ddl = for {
+
+    node <-
+      sql"""
+           |CREATE TABLE IF NOT EXISTS NODES(
+           |id BIGINT,
+           |remark VARCHAR,
+           |status BIGINT,
+           |logs VARCHAR,
+           |PRIMARY KEY (id),
+           |FOREIGN KEY (status) REFERENCES STATUS (id),
+           |FOREIGN KEY (logs) REFERENCES LOGS(id)
+           |);""".update.run
+
+    target <-
+      sql"""
+           |CREATE TABLE IF NOT EXISTS TARGET(
+           |id BIGINT,
+           				|host VARCHAR,
+           |target BIGINT,
+           |PRIMARY KEY(id),
+           |FOREIGN KEY(id) REFERENCES (nodes)
+           |);""".update.run
+
+
+    procedures <-
+      sql"""
+           |CREATE TABLE IF NOT EXISTS PROCEDURES(
+           |id BIGINT,
+           |name VARCHAR,
+           |remark VARCHAR,
+           |code VARCHAR,
+           |timeout BIGINT,
+           |PRIMARY KEY (id)
+           |);""".update.run
+
+    logs <-
+      sql"""
+           				 |CREATE TABLE IF NOT EXISTS LOGS(
+           				 |  lineID BIGINT,
+           				 |  lineVal VARCHAR,
+           				 |  nodeID BIGINT,
+           				 |  PRIMARY KEY(lineID),
+           				 |  FOREIGN KEY (nodeID) REFERENCES NODES(id)
+           				 |);""".update.run//TODO
+
+
+    telemetry <-
+      sql"""
+           |CREATE TABLE IF NOT EXISTS TELEMETRY(
+           |id BIGINT,
+           |arch VARCHAR,
+           |users BIGINT,
+           |processorCount BIGINT,
+           |loadAverage    BIGINT,
+           |memoryStatId   BIGINT,
+           |threadStatId   BIGINT,
+           |ifaceToNetStat VARCHAR,
+           |pathToDiskStat VARCHAR,
+           |PRIMARY KEY (id),
+           |FOREIGN KEY (id) REFERENCES NODES(id),
+           |FOREIGN KEY (memoryStatId) REFERENCES MEMORYSTAT(id),
+           |FOREIGN KEY (threadStatId) REFERENCES THREADSTAT(id),
+           |FOREIGN KEY (ifaceToNetStat) REFERENCES NETSTAT(iface),
+           |FOREIGN KEY (pathToDiskStat) REFERENCES MEMORYSTAT(id)
+           |);""".update.run
+
+    threadStat <-
+      sql"""
+           |CREATE TABLE IF NOT EXISTS THREADSTAT(
+           |id BIGINT,
+           |running BIGINT,
+           |sleeping BIGINT,
+           |stopped BIGINT,
+           |zombie  BIGINT,
+           |PRIMARY KEY (id),
+           |FOREIGN KEY (id) REFERENCES NODES(id)
+           |);""".update.run
+
+    netStat <-
+      sql"""
+           |CREATE TABLE IF NOT EXISTS NETSTAT(
+           |id BIGINT,
+           |iface VARCHAR,
+           |inet LONGVARCHAR,
+           |bcast LONGVARCHAR,
+           |mask  LONGVARCHAR,
+           |inet6 LONGVARCHAR,
+           |scope LONGVARCHAR,
+           |tx1    BIGINT,
+           |tx2    BIGINT,
+           |rx1    BIGINT,
+           |rx2    BIGINT,
+           |PRIMARY KEY (iface),
+           |FOREIGN KEY (id) REFERENCES NODES(id)
+           |);""".update.run
+
+    memoryStat <-
+      sql"""
+           |CREATE TABLE IF NOT EXISTS MEMORYSTAT(
+           |id BIGINT,
+           |totalVal BIGINT,
+           |totalUnit BIGINT,
+           |freeVal  BIGINT,
+           |freeUnit BIGINT,
+           |usedVal BIGINT,
+           |usedUnit BIGINT,
+           |cacheVal BIGINT,
+           |cacheUnit BIGINT,
+           |PRIMARY KEY (id),
+           |FOREIGN KEY (id) REFERENCES NODES(id)
+           |);""".update.run
+
+    diskStat <-
+      sql"""
+           |CREATE TABLE IF NOT EXISTS DISKSTAT(
+           |id BIGINT,
+           |freeVal BIGINT,
+           |freeUnits BIGINT,
+           |PRIMARY KEY (id),
+           |FOREIGN KEY (id) REFERENCES NODES(id)
+           |);""".update.run
+
+    status <-
+      sql"""
+           | CREATE TABLE IF NOT EXISTS STATUS(
+           | id BIGINT,
+           | statMessage VARCHAR,
+           | error VARCHAR,
+           | verdict VARCHAR,
+           | reason VARCHAR,
+           | PRIMARY KEY(id),
+           | FOREIGN KEY (id) REFERENCES NODES(id),
+           | FOREIGN KEY (telemetry) REFERENCES TELEMETRY(id)
+           | );""".update.run
+
+
+
+
+  } yield (nodes, procedures, logs, telemetry, threadStat, netStat, memoryStat, diskStat, target, status)
+
+
+
+
+  /** Procedures **/
+
+
+  implicit def nodeIdMeta: Meta[Id[Node]] = Meta[Long].xmap(Entity.id(_), _.toLong)
+  implicit def procedureIdMeta: Meta[Id[Procedure]] = Meta[Long].xmap(Entity.id(_), _.toLong)
+  implicit def durationMeta: Meta[Duration] = Meta[Long].xmap(Duration.ofHours(_), _.toHours)
+
+  def upsertProcedure(proc: Procedure): Task[Id[Procedure]] = {
+    val Procedure(procId, name, remark, code, timeout) = proc
+    sql"""insert into procedures(name, remark, code, timeout) values ($name , $remark, $code, $timeout)"""
+      .update
+      .withUniqueGeneratedKeys[Id[Procedure]]("id").transact(xa)
+//    procId match {
+//      case None =>
+//        sql"""insert into procedures(name,remark, code, timeout)values($name, $remark, $code, $timeout)"""
+//              .update
+//              .withUniqueGeneratedKeys[Id[Procedure]]("id").transact(xa)
+//      case Some(id) =>
+//        sql"""update procedures set name=$name, remark=$remark, code=$code, timeout=$timeout where id=$id """
+//              .update
+//              .withUniqueGeneratedKeys[Id[Procedure]]("id")
+//              .transact(xa)
+//    }
+  }
+
+//  def selectProcedures(): Task[Seq[Procedure]] = {
+//    sql"select id, name, remark, code, timeout from procedures".
+//      query[Procedure].
+//      to[Seq].transact(xa)
+//  }
+  //
+  //	class Procedures  extends ProcedureRepository {
+  //
+  //		def list(): Task[Seq[Entity.Id[Procedure]]] = {
+  //			sql"select * from PROCEDURES"
+  //				.query[Id[Procedure]]
+  //				.to[Seq]
+  //				.transact(xa)
+  //		}
+  //
+  //		def find(id: Entity.Id[Procedure]): Task[Option[Procedure]] = {
+  //			sql"select id, desc, code, duration from PROCEDURES where id=$id"
+  //				.query[Procedure]
+  //				.to[Option]
+  //				.transact(xa)
+  //		}
+  //
+  //		def delete(id: Entity.Id[Procedure]): Task[Entity.Id[Procedure]] = {
+  //			sql"delete from procedures where id"
+  //				.query[Procedure]
+  //				.to[Entity.Id]
+  //				.transact(xa)
+  //		}
+  //
+  //		def insert(p: Procedure): Entity.Id[Procedure] = {
+  //			upsertProcedure(Entity(p)).to[List].head
+  //		}
+  //
+  //		def update(id: Id[Procedure], f: Procedure => Procedure): Task[Id[Procedure]] = {
+  //			upsertProcedure(id)
+  //		}
+  //
+  //	}
+  //
+  //
+  //
+  //
+  //	/** Nodes **/
+  	def upsertNode(node: Node): Task[Id[Node]] = {
+  		// Note: ID can be none
+    val Node(idOp, target, remark, status, logs) = node
+    sql"""insert into nodes(remark) values ($remark)"""
+      .update
+      .withUniqueGeneratedKeys[Id[Node]]("id").transact(xa).map(id => id)
+//  		val Node(idOp, target, remark, _, logs) = node
+//  		val host = target.host
+//  		val port = target.port
+//  		(idOp) match {
+//  			case None     =>
+//  				sql"""insert into nodes(host, port, remark, logs) values ($host, $port, $remark, $logs)"""
+//  					.update
+//  					.withUniqueGeneratedKeys[Id[Node]]("id").transact(xa).map(id => id)
+//  			case Some(id) =>
+//  				sql"""update nodes set host=$host, port=$port, remark=$remark, logs=$logs where id=$id """
+//  					.update
+//  					.withUniqueGeneratedKeys[Id[Node]]("id")
+//  					.transact(xa)
+//  		}
+  	}
+
+  //
+  //	def selectNodes(): Task[Seq[(Long, String, Int, String, String)]] = {
+  //		sql"select id, host, port, remark, logs from nodes"
+  //			.query[(Long, String, Int, String, String)]
+  //			.to[Seq].transact(xa)
+  //	}
+  //	class Nodes extends NodeRepository {
+  //
+  //		def list(): Task[Seq[Id[Node]]] = {
+  //			sql"select id from nodes".query[Entity.Id[Node]].to[Seq].transact(xa)
+  //		}
+  //
+  //		def find(id: Entity.Id[Node]): Task[Option[Node]] = {
+  //			sql"select id, desc, code, duration from nodes where id=$id"
+  //				.query[Node]
+  //				.to[Option].transact(xa)
+  //		}
+  //
+  //		def delete(id: Id[Entity.Node]): Task[Entity.Id[Node]] = {
+  //			sql"delete from nodes where id=$id"
+  //				.query[Node]
+  //				.to[Entity.Id].transact(xa)
+  //		}
+  //
+  //		def insert(n: Node): Entity.Id[Node] = {
+  //			upsertNode(Entity(n)).to[List].head
+  //		}
+  //
+  //		def update(id: Id[Node], f: Node => Node): Task[Id[Node]] = {
+  //			upsertNode(id)
+  //		}
+  //
+  //		def ids: Observable[Set[Id[Node]]] = {
+  //			sql"select id from nodes"
+  //				.query[Id[Node]]
+  //				.to[Set].transact(xa).to[Observable]
+  //		}
+  //
+  //		def telemetries(nid: Id[Node]): Task[Option[TelemetrySeries]] = {
+  //			val n = nid.toLong
+  //			sql"select verdict from telemetry where id=$n"
+  //				.query[TelemetrySeries]
+  //				.to[Option].transact(xa)
+  //		}
+  //
+  //		def logs(nid: Id[Node]): Task[Option[LogSeries]] = {
+  //			val n = nid.toLong
+  //			sql"select lineID, lineVal from logs where nodeID=$n"
+  //				.query[LogSeries]
+  //				.to[Option].transact(xa)
+  //		}
+  //
+  //		//TODO: def meta(nid: Id[Entity.Node]): Task[Option[Node]]
+  //		//TODO: def telemetryDelta: Observable[Id[Entity.Node]]
+  //		//TODO: def logDelta: Observable[Id[Entity.Node]]
+  //		/*TODO:
+  //		def entities: Observable[Set[Node]] = ids
+  //			.switchMap { ns => Observable.fromTask(Task.traverse(ns)(find)) }
+  //			.map {_.flatten.toSet}
+  //
+  //		def persist(nid: Id[Entity.Node],
+  //					time: ZonedDateTime,
+  //					status: Status): Task[Failure | Id[Entity.Node]]
+  //		def persist(nid: Id[Entity.Node],
+  //					time: ZonedDateTime,
+  //					path: Path,
+  //					lines: Seq[Line]): Task[Failure | Id[Entity.Node]]
+  //			*/
+  //
+  //
+  //	}
+
+
+
+
+}
